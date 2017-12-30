61103280641b882f6b69b3c51fe10bad5742ba48
tone <tone4all@163.com>
2017-12-30 20:49:36 +0800

    支持多本书
    将多本书的信息写到bookInfo中

--- NovelCrawler/NovelCrawler/spiders/novel.py
+++ NovelCrawler/NovelCrawler/spiders/novel.py
@@ -8,15 +8,26 @@ xpathMap = {'title': '//*[@id="center"]/div[1]/h1/text()',
             'prev': '//*[@id="center"]/div[2]/a[1]//@href',
             "next": '//*[@id="center"]/div[2]/a[5]//@href'}
 
+# 最新章节 2017-12-30
+# https://www.miaobige.com/read/13395/11394345.html
+# title: 第三卷 帝国之路 第66章 陕西冒出的乱子
+
+bookInfo = {'挽明': (['https://www.miaobige.com/read/13395/11394345.html'],
+                   r'D:\Download\挽明.txt',
+                   'https://www.miaobige.com/read/13395/'),
+            '难道我是神': (['https://www.miaobige.com/read/18124/9706938.html'],
+                      r'D:\Download\难道我是神.txt',
+                      'https://www.miaobige.com/read/18124/'),
+            '大王饶命': (['https://www.miaobige.com/read/18491/9930529.html'],
+                      r'D:\Download\大王饶命.txt',
+                      'https://www.miaobige.com/read/18491/')
+            }
+bookName = '大王饶命'
 
 class NovelSpider(scrapy.Spider):
     name = 'novel'
     allowed_domains = ['www.miaobige.com']
-    start_urls = ['https://www.miaobige.com/read/13395/10130706.html']
-
-    # 最新章节 2017-12-30
-    #  https://www.miaobige.com/read/13395/11387310.html)
-    # title: 第三卷 帝国之路 第66章 陕西冒出的乱子
+    start_urls = bookInfo[bookName][0]
 
     def __init__(self):
         super(NovelSpider, self).__init__()
@@ -24,7 +35,7 @@ class NovelSpider(scrapy.Spider):
         # 用于测试时及时停止
         self.count = 0
         # 保存路径
-        self.path = r'D:\Download\挽明.txt'
+        self.path = bookInfo[bookName][1]
 
     def parse(self, response):
         selector = scrapy.Selector(response)
@@ -48,7 +59,7 @@ class NovelSpider(scrapy.Spider):
         print('title: ' + title)
         # print('content: ' + content)
 
-        next_href = 'https://www.miaobige.com/read/13395/' + next_href
+        next_href = bookInfo[bookName][2] + next_href
         print('next_href: ' + next_href)
 
         yield Request(next_href, callback=self.parse, dont_filter=True)
